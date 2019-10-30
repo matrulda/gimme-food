89273527d7098b6860b3804c91b9972e7612b81c
MatildaAslin <matilda.aslin@medsci.uu.se>
2019-10-30 21:19:59 +0100

    Make sure all relevent files are added to package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from gimme_food import __version__
 
 setup(name='gimme_food',
@@ -9,7 +9,10 @@ setup(name='gimme_food',
       author='Matilda Åslin',
       author_email='matilda@åslin.se',
       python_requires='>=3',
-      packages=find_packages(exclude=["tests"]),
+      packages=find_namespace_packages(),
+      package_data={'gimme_food': ['config/config.yaml', 'examples/recipe_1.json',
+                                   'examples/recipe_2.json']},
+      include_package_data=True,
       entry_points={
         'console_scripts': 'gimme_food = gimme_food.app:run_app'}
      )
